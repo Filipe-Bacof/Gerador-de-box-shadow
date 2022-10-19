502e95d1c3f5aad59a940e4bcfc93dd7d1f92df6
Filipe Bacof <filipebacof@gmail.com>
2022-10-19 18:52:20 -0300

    projeto mk1 finalizado

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ class BoxShadowGenerator {
         this.spreadRef.value = this.spread.value;
         this.blurRef.value = this.blur.value;
         this.applyRule();
-        this.showRule()
+        this.showRule();
     }
 
     applyRule() {
@@ -47,6 +47,24 @@ class BoxShadowGenerator {
         this.mozRule.innerText = this.currentRule;
     }
     
+    updateValue(type, value) {
+        switch(type) {
+            case "horizontal":
+                this.horizontalRef.value = value;
+            break;
+            case "vertical":
+                this.verticalRef.value = value;
+            break;
+            case "blur":
+                this.blurRef.value = value;
+            break;
+            case "spread":
+                this.spreadRef.value = value;
+            break;
+        }
+        this.applyRule();
+        this.showRule();
+    }
 }
 
 // Seleção de Elementos
@@ -83,3 +101,19 @@ const boxShadow = new BoxShadowGenerator(
 boxShadow.initialize();
 
 // Eventos
+horizontal.addEventListener("input", (e) => {
+    const value = e.target.value;
+    boxShadow.updateValue("horizontal", value)
+});
+vertical.addEventListener("input", (e) => {
+    const value = e.target.value;
+    boxShadow.updateValue("vertical", value)
+});
+blur.addEventListener("input", (e) => {
+    const value = e.target.value;
+    boxShadow.updateValue("blur", value)
+});
+spread.addEventListener("input", (e) => {
+    const value = e.target.value;
+    boxShadow.updateValue("spread", value)
+});
